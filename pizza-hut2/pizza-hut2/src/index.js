07bd5a280bc692e458fb66b8e84d7e751d89df6b
kingchan818 <61489623+kingchan818@users.noreply.github.com>
2021-11-08 02:21:57 +0800

    implemented react-redux on index page

--- pizza-hut2/pizza-hut2/src/index.js
+++ pizza-hut2/pizza-hut2/src/index.js
@@ -1,14 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { reducer } from './redux';
+import { Provider } from 'react-redux';
+const middelWare = [thunk];
+//const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
+// const initalState = {
+//     cart: { cartItems: cartItemsFromStorage },
+// };
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(...middelWare)));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 reportWebVitals();
